0e723856dba3f788e8f6df0a9abc61cec2bdc53d
Shaunak Kashyap <ycombinator@gmail.com>
2016-09-06 10:46:23 -0700

    Adding preamble

--- src/crypto.js
+++ src/crypto.js
@@ -1,3 +1,16 @@
+/**
+ * This module provides easy, yet strong, facilities for encrypting and decrypting serializable objects. The
+ * ease-of-use comes from the fact that this module is opinionated in its (strong) choice of cryptographic algorithms,
+ * lengths, and iterations that cannot be overriden by its users.
+ *
+ * The module exports a `makeCryptoWith` function that takes a single argument, an `opts` object. This object requires
+ * a property named `encryptionKey` which is a passphrase used by the encryption and decryption algorithms within
+ * this module. The `makeCryptoWith` function returns an object containing two functions, `encrypt` and `decrypt`.
+ *
+ * Both the `encrypt` and `decrypt` functions are inverses of each other and return Promises. That is:
+ *   someSerializableObj === await decrypt(await encrypt(someSerializableObj)).
+ */
+
 import crypto from 'crypto';
 
 const IV_LENGTH_IN_BYTES = 12;
